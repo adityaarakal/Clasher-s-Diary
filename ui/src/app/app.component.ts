15713b51b62b6dc2ca9c1f5343ea7894ee7aec0f
renusonaditya <renusonaditya@gmail.com>
2020-02-18 19:59:50 +0530

    18022020 eve

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +8,15 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'ui';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient/* , private clans: clans */) { }
 
   ngOnInit() {
+    let header = new HttpHeaders();
+    header.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU4OTk0YTg3LTYyMjktNGIzNS05OWUyLTNiMmUxZWFmZDk5NyIsImlhdCI6MTU4MjAyMTk0MCwic3ViIjoiZGV2ZWxvcGVyLzQzMDBjYmFlLWRlMDgtMzM2Yi0zYjZhLTdiYTRlMTI1NDJhMiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjE1Ny4zMy4xMjEuMjIwIl0sInR5cGUiOiJjbGllbnQifV19.mZnRfLPfiqmhkM3NKAvi9D01yO4pjhXV4wsOU3Mf75B5qLPsCSRmfef-6uzWf0LE1DzDYG0glmeVxpqRnNjiDg");
+    this.httpClient.post("https://api.clashofclans.com/v1/clans/", header)
+      .subscribe(
+        data => console.log("coc data => ", data)
+      )
   }
 
 }
